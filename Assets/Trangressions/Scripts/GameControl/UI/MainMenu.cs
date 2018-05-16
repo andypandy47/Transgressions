cf6da1d321414aafd5c59df152fb2cbfc51a5c9e
andypandy47 <akayaudio@gmail.com>
2018-05-16 17:26:02 +0100

    Revert "shit ui change"
    This reverts commit 3a2e1d43c6af0f2dbcff18a29d3644b4e940fa92.

--- Assets/Trangressions/Scripts/GameControl/UI/MainMenu.cs
+++ Assets/Trangressions/Scripts/GameControl/UI/MainMenu.cs
@@ -8,51 +8,25 @@ public class MainMenu : MonoBehaviour {
     public enum CurrentScreen
     {
         MainMenu,
-        SectionSelection,
-        MissionSelection,
+        MissionSelect,
         Help,
         Options
     }
     public CurrentScreen screen;
     GameObject instructionScreen;
-    GameObject sectionSelectionScreen;
-    public GameObject[] missionSelectScreens;
+    GameObject missionSelectScreen;
 
     private void Start()
     {
         instructionScreen = transform.GetChild(1).gameObject;
-        sectionSelectionScreen = transform.GetChild(2).gameObject;
-
+        missionSelectScreen = transform.GetChild(2).gameObject;
         screen = CurrentScreen.MainMenu;
     }
 
-    public void SectionSelect()
+    public void MissionSelect()
     {
-        sectionSelectionScreen.SetActive(true);
-        for (int i = 0; i < 2; i++)
-        {
-            if (!sectionSelectionScreen.transform.GetChild(i).gameObject.activeInHierarchy)
-                sectionSelectionScreen.transform.GetChild(i).gameObject.SetActive(true);
-            else
-                continue;
-        }
-        screen = CurrentScreen.SectionSelection;
-    }
-
-    public void Section1Select()
-    {
-        for (int i = 0; i < 2; i++)
-        {
-            sectionSelectionScreen.transform.GetChild(i).gameObject.SetActive(false);
-        }
-        missionSelectScreens[0].SetActive(true);
-        screen = CurrentScreen.MissionSelection;
-        print("Section 1 select scree");
-    }
-
-    public void Section2Select()
-    {
-        screen = CurrentScreen.MissionSelection;
+        missionSelectScreen.SetActive(true);
+        screen = CurrentScreen.MissionSelect;
     }
 
     public void Help()
@@ -68,21 +42,10 @@ public class MainMenu : MonoBehaviour {
             instructionScreen.SetActive(false);
             screen = CurrentScreen.MainMenu;
         }
-        else if (screen == CurrentScreen.SectionSelection)
+        else if (screen == CurrentScreen.MissionSelect)
         {
-            sectionSelectionScreen.SetActive(false);
+            missionSelectScreen.SetActive(false);
             screen = CurrentScreen.MainMenu;
-        }
-        else if (screen == CurrentScreen.MissionSelection)
-        {
-            for (int i = 0; i < missionSelectScreens.Length; i++)
-            {
-                if (missionSelectScreens[i].activeInHierarchy)
-                    missionSelectScreens[i].SetActive(false);
-                else
-                    continue;
-            }
-            SectionSelect();
         }
 
     }
